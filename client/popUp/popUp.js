ebbfbc4fa0deaf9df08c83110e2bcc3e383edbad
Yong Qian, Lee <b92101007@gmail.com>
2015-08-05 17:32:13 +0800

    完成基本的使用者登入登出、加入遊戲介面

---
+++ client/popUp/popUp.js
@@ -0,0 +1,34 @@
+//預設的彈跳視窗狀態
+Session.setDefault('popUpState', {
+  open: true
+});
+
+Template.popUp.helpers({
+  isHide: function() {
+    return (Session.get('popUpState').open ? '' : 'hide');
+  },
+  players: function() {
+    return APP.db.player.find({}, {sort: {sort: 1}});
+  },
+  isUserPlayer: function() {
+    return APP.isLoginUserInGame();
+  }
+});
+Template.popUp.events({
+  'click .close': function() {
+    var popUpState = Session.get('popUpState');
+    Session.set('popUpState', false);
+  },
+  'click button[data-action="join"]': function() {
+    Meteor.call('playerJoinGame');
+  },
+  'click button[data-action="exit"]': function() {
+    Meteor.call('playerExitGame');
+  },
+  'click button[data-action="loginIn"]': function() {
+    Meteor.loginWithFacebook();
+  },
+  'click button[data-action="loginOut"]': function() {
+    Meteor.logout();
+  }
+});
