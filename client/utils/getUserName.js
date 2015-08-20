eb3c4e0ca395fce9ad5ec41b22f1bf957faa21fa
Yong Qian, Lee <b92101007@gmail.com>
2015-08-20 21:25:42 +0800

    防止在已有使用者資料的狀態下重複訂閱使用者資料

--- client/utils/getUserName.js
+++ client/utils/getUserName.js
@@ -1,13 +1,16 @@
 //在創建模板時訂閱使用者暱稱資料
 Template.getUserName.onCreated(function() {
-  this.loadAction = Meteor.subscribe('userName', this.data.userId);
+  //若尚未訂閱此玩家資料，則訂閱之
+  if (!Meteor.users.findOne(this.data.userId)) {
+    this.loadAction = Meteor.subscribe('userName', this.data.userId);
+  }
 });
 //顯示使用者暱稱資料
 Template.getUserName.helpers({
   getUserName: function() {
     var instance = Template.instance();
     var user;
-    if (instance.loadAction.ready()) {
+    if (!instance.loadAction || instance.loadAction.ready()) {
       user = Meteor.users.findOne(this.userId);
       if (user && user.profile) {
         return '玩家' + user.profile.name;
