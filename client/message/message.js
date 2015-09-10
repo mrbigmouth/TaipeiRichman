968ca3b63cad7cd98540fbf245a779c28fedf491
Yong Qian, Lee <b92101007@gmail.com>
2015-09-10 21:20:42 +0800

    自動捲動卷軸的功能不用helper, 改為透過template.autorun實現

--- client/message/message.js
+++ client/message/message.js
@@ -1,6 +1,21 @@
 //預設不篩選訊息
 Session.setDefault('isFilterMessage', false);
 
+Template.message.onCreated(function() {
+  var instance = this;
+  //每當訊息資料有任何變動時，自動將捲軸捲到最底
+  instance.autorun(function() {
+    var $message = $( instance.firstNode );
+    //將#message區塊的捲軸捲到最底
+    $message.scrollTop( $message.children('ol').height() + 100 );
+    //訊息資料庫有變動時會自動重新執行
+    APP.db.message.getAllOrderByTime().fetch();
+    //使用者切換篩選訊息模式時會自動重新執行
+    Session.get('isFilterMessage');
+  })
+});
+
+//訊息區塊的helpers
 Template.message.helpers({
   //判斷使用者是否能夠進行聊天
   userCanChat: function() {
@@ -20,17 +35,6 @@ Template.message.helpers({
     if (this.isChatMessage()) {
       return 'background:#999999;';
     }
-  },
-  //每當訊息資料有任何變動時，自動將捲軸捲到最底
-  makeScrollToBottom: function() {
-    var $message = $( Template.instance().firstNode );
-    //將#message區塊的捲軸捲到最底
-    $message.scrollTop( $message.children('ol').height() + 100 );
-    //訊息資料庫有變動時會自動重新執行
-    APP.db.message.getAllOrderByTime().fetch();
-    //使用者切換篩選訊息模式時會自動重新執行
-    Session.get('isFilterMessage');
-    return;
   }
 });
 
