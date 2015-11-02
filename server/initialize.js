9f1db0acea9d976c807e9c0f68e43b87c979570f
mrbigmouth <b92101007@gmail.com>
2015-11-02 13:43:17 +0800

    修正遊戲訊息沒有隨著伺服器重啟初始化的bug

--- server/initialize.js
+++ server/initialize.js
@@ -4,5 +4,7 @@ APP.gameStatus.initialize();
 APP.db.landMark.initialize();
 //重新初始化當前玩家資料
 APP.db.player.initialize();
+//重新初始化遊戲訊息資料
+APP.db.message.initialize();
 //將APP啟動狀態設為已啟動
 APP.initialized.set(true);
