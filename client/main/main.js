564a39b781f4507d9f54abcef4651c6cb7d62865
mrbigmouth <b92101007@gmail.com>
2015-12-03 16:24:43 +0800

    在主畫面上顯示及時玩家資訊

--- client/main/main.js
+++ client/main/main.js
@@ -1,4 +1,8 @@
 Template.main.helpers({
+  //判斷是否遊戲已開始
+  isGameStart: function() {
+    return APP.gameStatus.isGameStart();
+  },
   //判斷使用者是否能看到"動作"按鈕
   canAction: function() {
     var gameStatus = APP.gameStatus;
@@ -20,5 +24,12 @@ Template.main.events({
       open: true,
       content: e.currentTarget.id
     });
+  }
+});
+
+//取得玩家資訊
+Template.playerInfo.helpers({
+  players: function() {
+    return APP.db.player.getAllOrderBySort();
   }
 });
